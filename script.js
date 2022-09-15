0225e945ae7dcc559134c5709bc98b0a01e20714
Meen Philip <meenphilip@gmail.com>
2022-09-15 21:23:35 +0300

    add event listeners and calculate function

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const amountEl_two = document.getElementById("amount-two");
 
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+  console.log("meen");
+}
+
+// Event Listeners
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
+
+calculate();
